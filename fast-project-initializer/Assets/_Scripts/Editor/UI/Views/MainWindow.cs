45c9bf2edafab5f1b830f06c51b1682bce729262
m.winiarczyk <mateusz.winiarczyk@outlook.com>
2021-04-24 19:13:38 +0200

    Create MainWindow
    Main window is a starting point of the tool.

---
+++ fast-project-initializer/Assets/_Scripts/Editor/UI/Views/MainWindow.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using UnityEngine;
+using Zenject;
+
+namespace PushForce.FastProjectInitializer.UI
+{
+	public class MainWindow : ZenjectEditorWindow
+	{
+		public const string TITLE = "Fast Project Initializer";
+		
+		private readonly IView directoryInitializerView = new DirectoryInitializerView();
+		
+		public override void InstallBindings()
+		{}
+		
+		[MenuItem("Window/PushForce/FastProjectInitializer")]
+		public static void OpenWindow()
+		{
+			var window = GetWindow<MainWindow>();
+			window.titleContent = new GUIContent(TITLE);
+			window.Show();
+		}
+
+		public override void OnGUI()
+		{
+			base.OnGUI();
+			directoryInitializerView.drawGUI();
+		}
+	}
+}
