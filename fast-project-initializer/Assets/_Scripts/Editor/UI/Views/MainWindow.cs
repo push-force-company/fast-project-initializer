a7193c58727dd4802d7b52cfd0d50f0e21bcb7fc
m.winiarczyk <mateusz.winiarczyk@outlook.com>
2021-04-25 13:49:03 +0200

    Add directory and hierarchy view installers

--- fast-project-initializer/Assets/_Scripts/Editor/UI/Views/MainWindow.cs
+++ fast-project-initializer/Assets/_Scripts/Editor/UI/Views/MainWindow.cs
@@ -1,3 +1,4 @@
+using PushForce.FastProjectInitializer.Installers;
 using UnityEditor;
 using UnityEngine;
 using Zenject;
@@ -6,10 +7,18 @@ namespace PushForce.FastProjectInitializer.UI
 {
 	public class MainWindow : ZenjectEditorWindow
 	{
-		private readonly IView directoryInitializerView = new DirectoryInitializerView();
+		[Inject]
+		private readonly IView[] views;
+		private readonly string[] toolbarNames = {"Directory Initialization", "Hierarchy Initialization"};
+		private IToolbarPanel toolbarPanel;
 		
 		public override void InstallBindings()
-		{}
+		{
+			DirectoryInitializerViewInstaller.Install(Container);
+			HierarchyInitializerViewInstaller.Install(Container);
+			Container.Inject(this);
+			toolbarPanel = new ToolbarPanel(views, toolbarNames);
+		}
 		
 		[MenuItem("Window/PushForce/FastProjectInitializer")]
 		public static void OpenWindow()
